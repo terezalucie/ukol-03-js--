ded3346a4c17c45ad73a167c2a9d080f989e1245
Tereza Kolaříková <tereza.lucie@protonmail.com>
2024-05-23 20:03:22 +0200

    05

--- src/componenst/Header/header.jsx
+++ src/componenst/Header/header.jsx
@@ -3,13 +3,13 @@ import "./header.css"
 export const Header = () => {
     return (
         <header>
-            <div className="panel" style={{ backgroundImage: "url('https://apps.kodim.cz/daweb/trening-api/assets/house01.jpg')" }}><a href="http://localhost:5173/dum01.html">Dřevostavba v japonském stylu</a>
+            <div className="panel" style={{ backgroundImage: "url('https://apps.kodim.cz/daweb/trening-api/assets/house01.jpg')" }}><a href="/dum01.html">Dřevostavba v japonském stylu</a>
             </div>
-            <div className="panel" style={{ backgroundImage: "url('https://apps.kodim.cz/daweb/trening-api/assets/house02.jpg')" }}><a href="http://localhost:5173/dum02.html">Vilka na předměstí</a>
+            <div className="panel" style={{ backgroundImage: "url('https://apps.kodim.cz/daweb/trening-api/assets/house02.jpg')" }}><a href="/dum02.html">Vilka na předměstí</a>
             </div>
-            <div className="panel" style={{ backgroundImage: "url('https://apps.kodim.cz/daweb/trening-api/assets/house03.jpg')" }}><a href="http://localhost:5173/dum03.html">Moderní dům s bazénem</a>
+            <div className="panel" style={{ backgroundImage: "url('https://apps.kodim.cz/daweb/trening-api/assets/house03.jpg')" }}><a href="/dum03.html">Moderní dům s bazénem</a>
             </div>
-            <div className="panel" style={{ backgroundImage: "url('https://apps.kodim.cz/daweb/trening-api/assets/house04.jpg')" }}><a href="http://localhost:5173/dum04.html">Útulný domeček</a>
+            <div className="panel" style={{ backgroundImage: "url('https://apps.kodim.cz/daweb/trening-api/assets/house04.jpg')" }}><a href="/dum04.html">Útulný domeček</a>
             </div>
         </header>
     )
